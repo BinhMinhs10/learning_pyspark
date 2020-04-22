e1eefd56ca242bdd6d9853824570abbc5fda78fb
BinhMinhs10 <nguyenbinhminh07101997@gmail.com>
2020-04-22 17:50:29 +0700

    resolve graphdataframes

---
+++ graphAnalytics.py
@@ -0,0 +1,50 @@
+#  $SPARK_HOME/bin/spark-submit --packages graphframes:graphframes:0.3.0-spark2.0-s_2.11 graphAnalytics.py
+
+from pyspark.sql import SparkSession
+import pyspark
+from graphframes import GraphFrame
+from pyspark.sql.functions import desc
+
+spark = SparkSession \
+    .builder \
+    .appName("Python Spark SQL basic example")\
+    .getOrCreate()
+spark.sparkContext.setLogLevel("WARN")
+
+bikeStations = spark.read.option("header", "true") \
+    .csv("data/bike-data/201508_station_data.csv")
+# print(bikeStations.collect())
+
+tripData = spark.read.option("header", "true") \
+    .csv("data/bike-data/201508_trip_data.csv")
+tripData.show(5)
+
+stationVertices = bikeStations.withColumnRenamed("name", "id").distinct()
+tripEdges = tripData\
+    .withColumnRenamed("Start Station", "src") \
+    .withColumnRenamed("End Station", "dst")
+
+stationGraph = GraphFrame(stationVertices, tripEdges)
+stationGraph.cache()
+
+print("\nTotal Number of Station: " + str(stationGraph.vertices.count()))
+print("\nTotal Number of Trips in Graph: " + str(stationGraph.edges.count()))
+print("\nTotal Number of Trips in Original Data: "+ str(tripData.count()))
+
+# Query in graph
+# stationGraph.edges.groupBy("src", "dst").count()\
+    # .orderBy(desc("count")).show()
+
+stationGraph.edges \
+    .where("src = 'Townsend at 7th' OR dst = 'Townsend at 7th'") \
+    .groupBy("src", "dst").count() \
+    .orderby(desc("count")) \
+    .show(10)
+
+# # sub graph
+# townAnd7thEdges = stationGraph.edges.where("src = 'Townsend at 7th' OR dst = 'Townsend at 7th'")
+# subgraph = GraphFrame(stationGraph.vertices, townAnd7thEdges)
+
+# motif finding
+
+print("\nDONE======================\n")
